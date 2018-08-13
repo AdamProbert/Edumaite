58ddd6631b1b873055afb572789d3e29322d85b9
Adam <adamprobert@live.co.uk>
2018-08-13 21:18:07 +0100

    Added navigational drawer. Screen switching is done with fragments and one activity. Super smooth!

--- app/src/main/java/com/edumaite/adam/edumiate_poc/MainActivity.java
+++ app/src/main/java/com/edumaite/adam/edumiate_poc/MainActivity.java
@@ -4,19 +4,104 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import junit.framework.Test;
 
-    Context context;
+public class MainActivity extends AppCompatActivity
+        implements TestFragment2.OnFragmentInteractionListener,
+        TestFragment1.OnFragmentInteractionListener{
+
+    private Context context;
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        fragmentClass = TestFragment1.class;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+
+                        item.setChecked(true);
+
+                        // Handle navigation view item clicks here.
+                        int id = item.getItemId();
+                        Fragment fragment = null;
+                        Class fragmentClass = null;
+                        if (id == R.id.bot) {
+                            fragmentClass = TestFragment1.class;
+                        } else if (id == R.id.yoda) {
+                            fragmentClass = TestFragment2.class;
+                        } else {
+                            return true;
+                        }
+
+                        try {
+                            fragment = (Fragment) fragmentClass.newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+                        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                        drawer.closeDrawer(GravityCompat.START);
+                        return true;
+                    }
+                });
+
         context=this;
         isInternetOn();
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public boolean isInternetOn() {
@@ -53,5 +138,11 @@ public class MainActivity extends Activity {
             return false;
         }
     }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
 }
 
