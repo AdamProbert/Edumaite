9602f5f47f0fa809fdaa0a1f55f7251a3c2cc719
Adam <adamprobert@live.co.uk>
2018-10-20 16:30:58 +0100

    Student attendance list view done

--- app/src/main/java/com/edumaite/adam/edumiate_poc/StudentsFragment.java
+++ app/src/main/java/com/edumaite/adam/edumiate_poc/StudentsFragment.java
@@ -4,9 +4,18 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import com.edumaite.adam.edumiate_poc.adapters.RecyclerItemClickListener;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+
+import java.util.List;
 
 
 /**
@@ -17,15 +26,7 @@ import android.view.ViewGroup;
  * Use the {@link StudentsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class StudentsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class StudentsFragment extends ListFragment implements OnItemClickListener{
 
     private OnFragmentInteractionListener mListener;
 
@@ -44,27 +45,30 @@ public class StudentsFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static StudentsFragment newInstance(String param1, String param2) {
         StudentsFragment fragment = new StudentsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.Students, android.R.layout.simple_list_item_checked);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_students, container, false);
+        View v =  inflater.inflate(R.layout.fragment_students, container, false);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -91,16 +95,10 @@ public class StudentsFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
